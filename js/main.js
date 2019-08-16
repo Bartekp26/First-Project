58d2e580e070864dbf6ac0e9520019ddc3657ba5
Bartek Płaniak <bartek.planiak@gmail.com>
2019-08-16 13:05:32 +0200

    Add arrow functions

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@ const linksContainer = document.querySelector('.nav');
 const links = document.querySelectorAll('.nav-item');
 
 for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function () {
+    links[i].addEventListener('click', () => {
         document.querySelector('.active').classList.remove('active');
         this.classList.add('active');
     }, false);
 }
 
-
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', () => {
     if(window.scrollY > 100){
         document.querySelector('.navbar').classList.add('black');
         document.querySelector('.navbar-collapse').classList.remove('show');
@@ -22,10 +21,9 @@ window.addEventListener('scroll', function(){
 let showMoreButton = document.querySelector('div.showMore');
 const noneElements = document.querySelectorAll('div.d-none');
 
-
-function showMore(e){
+const showMore = e => {
     const target = e.target;
-    
+
     if(showMoreButton.textContent == "Show more"){
         for(let i = 0; i < noneElements.length; i++){
 
@@ -43,9 +41,6 @@ function showMore(e){
     } 
 }
 
-
 showMoreButton.addEventListener('click', showMore, false);
-
-
 
 var lightbox1 = $('.gallery a').simpleLightbox();
